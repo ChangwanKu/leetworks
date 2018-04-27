62b2d19d0d1c8bc2edc9b74b55c66dd945656039
Chang Wan Ku <ku7078@gmail.com>
2018-04-27 13:07:31 +0900

    testing testing

--- sexbarplot.R
+++ sexbarplot.R
@@ -15,3 +15,7 @@ a3 <- a2 %>% mutate(group=substr(key,1,1),year=substr(key,2,3))
 a3
 ggplot(a3,aes(x=year, y=value, fill = group)) + geom_bar(stat='identity', position = 'dodge')
 
+#testing testing
+
+
+#r studio에서 작성한 걸 바로 commit 하는 것을 테스트해보자 
