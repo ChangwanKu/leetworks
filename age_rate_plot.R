7e35167cec9976d9be31b7808c3105f823b3b824
itwill009 <a01084830885@gmail.com>
2018-04-30 16:18:28 +0900

    change line2bar

--- age_rate_plot.R
+++ age_rate_plot.R
@@ -8,5 +8,5 @@ csv <- read.csv('./rawdatas/age.csv')
 csv2 <- csv %>% filter(schoolindex == 1) %>% select(ends_with('14'),ends_with('15'),ends_with('16'),ends_with('17')) %>% gather()
 csv3 <- csv2 %>% mutate(group = substr(key,1,1),year = substr(key,2,3))
 
-p <- csv3 %>% ggplot(aes(year,value,group=group,color=group)) + geom_line() + geom_point()
+p <- csv3 %>% ggplot(aes(year,value,fill=group)) + geom_bar(stat = 'identity')
 p + theme_classic()
