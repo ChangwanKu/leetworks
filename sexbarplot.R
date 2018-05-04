fb76fc47fd39db2a5887013c4d4fb0803ab17b37
itwill009 <a01084830885@gmail.com>
2018-05-04 22:03:03 +0900

    sex it commit

--- sexbarplot.R
+++ sexbarplot.R
@@ -16,10 +16,10 @@ a3 <- a2 %>% mutate(group=substr(key,1,1),year=substr(key,2,3))
 a3
 p <- ggplot(a3,aes(x=year, y=value, fill = group)) + geom_bar(stat='identity', position = 'dodge', width = 0.5)
 
-p + geom_text(aes(x=year, y=value, label = paste0(value)),size=4,vjust=-0.5, position = position_dodge(0.5)) + 
-  theme_light() + labs(title='남녀비율') + 
+p + geom_text(aes(x=year, y=value, label = paste0(value)),size=10,vjust=-0.5, position = position_dodge(0.5)) + 
+  labs(title='남녀비율') + 
   theme(legend.position="bottom", legend.direction="horizontal", legend.title = element_blank()) + 
   scale_fill_manual(values = c('#FF00FF','#FFCCFF'), labels = c('남','여')) +
-  scale_x_discrete(labels=seq(2014,2017,1)) + scale_y_continuous(breaks = NULL, expand = c(0,0), limits = c(0,32)) + 
-  theme(axis.title.x = element_blank(), axis.title.y = element_blank(), axis.ticks = element_blank(), panel.grid = element_blank() )
+  scale_x_discrete(labels=seq(2014,2017,1)) + 
+  theme(axis.title.x = element_blank(), axis.title.y = element_blank(), panel.grid.major.x = element_blank(), panel.grid.major.y = element_line(color = 'gray'), panel.grid.minor.y = element_line(color='gray'), axis.ticks = element_blank(), plot.title = element_text(face='bold',hjust=0), panel.background = element_blank(), axis.text.y = element_blank(), axis.text.x = element_text(size = 20) )
                                                                                                                      
