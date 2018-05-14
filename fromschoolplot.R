92e8878e0b55213225c81ef7796f48565af5cf57
gomguard <34765216+gomguard@users.noreply.github.com>
2018-05-14 13:52:21 +0900

    2019 ADD

--- fromschoolplot.R
+++ fromschoolplot.R
@@ -7,7 +7,6 @@ library(readr)
 fromschool_data <- read_csv('./rawdatas/fromschool.csv')
 
 
-
 # selected_colname_fromschool<- NULL
 # for (year in 10:as.numeric(substr(year_value,3,4))-1) {
 #   selected_colname_fromschool <- c(selected_colname_fromschool, paste("y",year, sep=""))
@@ -16,10 +15,11 @@ fromschool_data <- read_csv('./rawdatas/fromschool.csv')
 
 school_index <-1
 
-selected_data_fromschool <- fromschool_data %>% filter(schoolindex == 3) %>% group_by(school) %>%
+selected_data_fromschool <- fromschool_data %>% filter(schoolindex == 1) %>% group_by(school) %>%
   mutate(tot = sum(y9, y10, y11, y12, y13, y14, y15, y16, y17)) %>% arrange(desc(tot)) %>% distinct(school,tot)
-
+selected_data_fromschool
 top5 <- selected_data_fromschool %>%  head(5)
+
 
 etc <- data.frame(school='기타',tot=sum(selected_data_fromschool$tot)-sum(top5$tot))
 
@@ -35,13 +35,22 @@ text_y <- data.frame(pos = rev(cumsum(fromschool$tot)-fromschool$tot/2))
 
 p <- ggplot(from,aes(x='',y=tot,fill=school)) + geom_bar(width = 1, size =1, color = 'white', stat = 'identity')
 p <- p+coord_polar('y', start = 0)
+
+
 p <- p + theme_minimal() + theme(axis.title = element_blank(), axis.text = element_blank(), panel.grid = element_blank(), legend.position = 'none')
+
 # p <- p + scale_fill_grey(start = 0.8, end = 0.1)
+<<<<<<< HEAD
 p <- p + scale_fill_brewer()
+
+p + geom_text(aes(label=paste(school,tot), y = text_y), nudge_x = 0.7, size = 6 )
+=======
+p <- p + scale_fill_manual(values = c("#FF7FFF", "#FF66FF", "#FF4CFF", "#FF33FF", "#FF1CFF", "#FF00FF"))
 p + geom_text(aes(label=paste(school,tot), y = text_y), nudge_x = 0.7 )
+>>>>>>> 911b10c716a64543bd77f7bb570f35553ba7adf9
 
 
-# scale_fill_manual(values = c('#ffccff', '#ffcccc', '#ff99cc','#ff00ff','#ff00cc','#660033'))
+# scale_fill_manual(values = c("#FF00FF", "#FF1CFF", "#FF33FF", "#FF4CFF", "#FF66FF", "#FF7FFF"))
 ##################################################################################################여기까지
 # etc_label <- selected_data_fromschool %>% tail(sum(tally(selected_data_fromschool)$n)-5)
 # etc_label <- sprintf("%s(%s)", etc_label$school, etc_label$sum)
@@ -57,5 +66,5 @@ p + geom_text(aes(label=paste(school,tot), y = text_y), nudge_x = 0.7 )
 
 etc_label <- selected_data_fromschool %>% tail(-5)
 
-a = paste(etc_label$school,'(',etc_label$tot,')', collapse = ', ')
+a = paste(etc_label$school,'(',etc_label$tot,')', collapse = ', ', sep="")
 print(a)
